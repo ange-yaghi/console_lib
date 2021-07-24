4edba935bc1f9e6ba50abdd06ecb9cbc03ece98e
Ange Yaghi <ange.yaghi@outlook.com>
2021-07-24 16:54:32 -0400

    Test commit

---
+++ include/test.h
@@ -0,0 +1,6 @@
+#ifndef CONSOLE_LIB_TEST_H
+#define CONSOLE_LIB_TEST_H
+
+int hello_world();
+
+#endif /* CONSOLE_LIB_TEST_H */
---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+#include "../include/test.h"
+
+#include <iostream>
+
+int hello_world() {
+    std::cout << "Hello world\n";
+
+    return 0;
+}
